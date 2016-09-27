7fe398f7c586747489cd6b1e8912786b3c8f166b
juandjara <juanorigami@gmail.com>
2016-09-28 00:16:35 +0200

    search

---
+++ search.js
@@ -0,0 +1,17 @@
+module.exports = function(req, res){
+  var search = require("youtube-search");
+  var env    = require("./env");
+
+  var query = req.query.q;
+  var options  = {
+    key: env.youtube,
+    maxResults: 10
+  };
+  search(query, options, function(err, results){
+    if(err){
+      return res.status(500).send(err);
+    }
+
+    res.json(results);
+  });
+}
