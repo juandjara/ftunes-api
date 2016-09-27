c8e44129286301aa233a6a353dbd0cdcbfbf67fc
juandjara <juanorigami@gmail.com>
2016-09-27 23:24:44 +0200

    many things

--- converter.js
+++ converter.js
@@ -4,6 +4,7 @@ module.exports = function(req, res, next){
 
   var id     = req.params.ytid;
   var title  = req.params.title;
+  var isDownload = req.query.dl;
 
   if(!id || !title){
     return res.status(400).send("Missing param ytid or title");
@@ -14,25 +15,42 @@ module.exports = function(req, res, next){
     filter: "audioonly"
   });
 
-  res.setHeader('Content-disposition', 'attachment; filename='+title+'.mp3');
+  if(isDownload){
+    res.setHeader('Content-disposition', 'attachment; filename='+title+'.mp3');
+  }
   res.setHeader('Content-type', 'audio/mpeg');
 
-  var proc = new ffmpeg({source: stream});
-  proc.withAudioCodec('libmp3lame')
-    .toFormat('mp3')
-    .output(res)
-    .run();
+  stream.on('info', function(info){
+    //res.setHeader('Content-Length', info.size);
+    try{
+      convert(res, stream);      
+    }catch(err){
+      return res.status(500).send(err);
+    }
+  })
 
-  var initTime, endTime;
-  proc.on('start', function(){
-    initTime = Date.now();
-    console.log('Start streaming file '+title+'.mp3 from url '+url);
-  });
-  proc.on('end', function(){
-    endTime = Date.now();
-    var time = (endTime - initTime) / 1000;
-    console.log('finished streaming file '+title+'.mp3 from url '+url);
-    console.log('Streaming took '+time.toFixed(3)+'s');
-  });
-  
+  function convert(res, stream){
+    var proc = new ffmpeg({source: stream});
+    proc.withAudioCodec('libmp3lame')
+      .toFormat('mp3')
+      .pipe(res);
+
+    var initTime = Date.now(); 
+    var endTime  = initTime;
+    proc.on('start', function(){
+      console.log('Start streaming file '+title+'.mp3 from url '+url);
+    });
+    proc.on('end', function(){
+      if(isDownload){
+        endTime = Date.now();
+        var time = (endTime - initTime) / 1000;
+        console.log('Streaming took '+time.toFixed(3)+'s');
+      }
+      console.log('finished streaming file '+title+'.mp3 from url '+url);
+    });
+    proc.on('error', function(){
+      res.end();
+    });
+  }
+
 }
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var app     = express();
 var cors    = require('cors');
+var path    = require('path');
 var yt_mp3  = require('./converter.js');
 
 app.set('json spaces', 2);
 app.use(cors());
 
 app.get('/', function(req, res){
-    res.json({status: 'ok',
-              description: 'descripcion del servicio',
-              sample:'/url-ejemplo-servicio'});
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/dl/:ytid/:title', yt_mp3);
+app.get('/api', function(req, res){
+    res.json({status: 'ok',
+              description: 'convertir musica de yt a mp3',
+              sample:'/dl/:yt_video_id/:titulo_de_la_descarga'});
+});
+app.get('/api/:ytid/:title', yt_mp3);
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
