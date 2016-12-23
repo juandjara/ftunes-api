21c038029ceec5ccece0ea666840cc9fb20c193a
juandjara <juanorigami@gmail.com>
2016-12-23 23:43:13 +0100

    refactor: api purpose

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ var app     = express();
 var cors    = require('cors');
 var downloader = require('./downloader');
 var streamer   = require('./streamer');
-var search  = require('./search');
+var search     = require('./search');
 var sendSeekable = require('send-seekable');
 
 app.set('json spaces', 2);
 app.use(cors());
 app.use(sendSeekable);
 
-app.use(express.static('public'));
-
-app.get('/api', function(req, res){
-    res.json({status: 'ok',
-              description: 'convertir musica de yt a mp3',
-              sample:'/dl/:yt_video_id/:titulo_de_la_descarga'});
+app.get('/', function(req, res){
+  res.json({
+    status: 'ok',
+    description: 'convertir musica de yt a mp3',
+    sample:'/dl/:yt_video_id/:titulo_de_la_descarga'
+  });
 });
-app.get('/api/dl/:ytid', downloader);
-app.get('/api/stream/:ytid', streamer);
-app.get('/api/search', search);
+app.get('/dl/:ytid', downloader);
+app.get('/stream/:ytid', streamer);
+app.get('/search', search);
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
