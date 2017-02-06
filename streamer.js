74b8307a1a2aec625b14651f959b719ab92a8b0d
juandjara <juanorigami@gmail.com>
2017-02-06 22:03:03 +0100

    update ytdl-core and add error handling

--- streamer.js
+++ streamer.js
@@ -6,11 +6,12 @@ module.exports = function(req, res, next){
     return res.status(400).json({error: "Missing param ytid"});
   }
 
-  var url    = "https://youtube.com/watch?v="+id;
+  var url   = "https://youtube.com/watch?v="+id;
   var video = ytdl(url, {
     filter: "audioonly"
   });
 
+  video.on('error', err => res.status(500).send(err))
   video.on('response', function(data){
     var length = parseInt(data.headers["content-length"]);
     res.sendSeekable(video, {
